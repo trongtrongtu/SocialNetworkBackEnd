435141707ecb5b57ac7d3ec621f926a0428e7b84
trongtrongtu <trongtrongtu169@gmail.com>
2020-11-01 23:33:51 +0700

    cteate notification and post

--- routes/notification.js
+++ routes/notification.js
@@ -2,7 +2,10 @@ var router = global.router;
 let Notification = require('../models/Notification');
 let Like = require('../models/Like');
 let Post = require('../models/Post');
+let Follow = require('../models/Follow');
+let Comment = require('../models/Comment');
 var mongoose = require('mongoose');
+const User = require('../models/User');
 
 router.get('/list_all_notification', (request, response) => {
   Notification.find({}).limit(100).sort({ user: 1 }).select({
@@ -25,48 +28,101 @@ router.get('/list_all_notification', (request, response) => {
     } else {
       let dataArr = []
       notifications.map((item, index) => {
-        let notificationsId = item.like
-        Like.findOne({ _id: notificationsId }).limit(100).sort({ user: 1 }).select({
-          user: 1,
-          post: 1,
-        }).exec((err, like) => {
-          if (like && like._id) {
-            let likeId = like.post
-            Post.findOne({ _id: likeId }).limit(100).sort({}).select({
-              image: 1,
-            }).exec((err, post) => {
-              like['post'] = post
-              item['like'] = like;
-              dataArr.push(item)
-              if (dataArr.length == notifications.length) {
-                response.json({
-                  result: "ok",
-                  data: {
-                    getUserNotifications: {
-                      count: `${notifications.length}`,
-                      notifications: dataArr,
-                      __typename: "NotificationsPayload"
-                    }
-                  },
-                  messege: "Query room successfully"
-                });
-              }
+        let userId = item.author
+        User.findOne({ _id: userId }).limit(100).sort({}).select({
+          fullName: 1,
+          username: 1,
+          image: 1,
+        }).exec((err, user) => {
+          item['author'] = user;
+          if (item.like) {
+            let likeId = item.like
+            Like.findOne({ _id: likeId }).limit(100).sort({ user: 1 }).select({
+              user: 1,
+              post: 1,
+            }).exec((err, like) => {
+              let likeId = like.post
+              Post.findOne({ _id: likeId }).limit(100).sort({}).select({
+                image: 1,
+              }).exec((err, post) => {
+                like['post'] = post
+                item['like'] = like;
+                dataArr.push(item)
+                if (dataArr.length == notifications.length) {
+                  response.json({
+                    result: "ok",
+                    data: {
+                      getUserNotifications: {
+                        count: `${notifications.length}`,
+                        notifications: dataArr,
+                        __typename: "NotificationsPayload"
+                      }
+                    },
+                    messege: "Query room successfully"
+                  });
+                }
+              })
             })
-          } else {
-            dataArr.push(item)
-            if (dataArr.length == notifications.length) {
-              response.json({
-                result: "ok",
-                data: {
-                  getUserNotifications: {
-                    count: `${notifications.length}`,
-                    notifications: dataArr,
-                    __typename: "NotificationsPayload"
-                  }
-                },
-                messege: "Query room successfully"
-              });
-            }
+          } else if (item.follow) {
+            let followId = item.follow
+            Follow.findOne({ _id: followId }).limit(100).sort({ user: 1 }).select({
+              user: 1,
+              follower: 1,
+            }).exec((err, follow) => {
+              let userId = follow.user
+              User.findOne({ _id: userId }).limit(100).sort({}).select({
+                fullName: 1,
+                username: 1,
+                image: 1,
+              }).exec((err, user) => {
+                follow['user'] = user
+                item['follow'] = follow;
+                dataArr.push(item)
+                if (dataArr.length == notifications.length) {
+                  response.json({
+                    result: "ok",
+                    data: {
+                      getUserNotifications: {
+                        count: `${notifications.length}`,
+                        notifications: dataArr,
+                        __typename: "NotificationsPayload"
+                      }
+                    },
+                    messege: "Query room successfully"
+                  });
+                }
+              })
+            })
+          } else if (item.comment) {
+            let commentId = item.comment
+            Comment.findOne({ _id: commentId }).limit(100).sort({ user: 1 }).select({
+              comment: 1,
+              post: 1,
+              author: 1,
+            }).exec((err, comment) => {
+              let postId = comment.post
+              Post.findOne({ _id: postId }).limit(100).sort({}).select({
+                title: 1,
+                image: 1,
+              }).exec((err, post) => {
+                comment['post'] = post
+                item['comment'] = comment;
+                dataArr.push(item)
+                if (dataArr.length == notifications.length) {
+                  response.json({
+                    result: "ok",
+                    data: {
+                      getUserNotifications: {
+                        count: `${notifications.length}`,
+                        notifications: dataArr,
+                        __typename: "NotificationsPayload"
+                      }
+                    },
+                    messege: "Query room successfully"
+                  });
+                }
+              })
+            })
           }
         })
       })
--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,9 @@
 var router = global.router;
 let Post = require('../models/Post');
+let Like = require('../models/Like');
+let Follow = require('../models/Follow');
+let Comment = require('../models/Comment');
+const User = require('../models/User');
 var mongoose = require('mongoose');
 
 router.get('/list_all_post', (request, response) => {
@@ -10,7 +14,7 @@ router.get('/list_all_post', (request, response) => {
     author: 1,
     likes: 1,
     comments: 1,
-  }).exec((err, post) => {
+  }).exec((err, postArr) => {
     if (err) {
       response.json({
         result: "failed",
@@ -18,11 +22,66 @@ router.get('/list_all_post', (request, response) => {
         messege: `Error is : ${err}`
       });
     } else {
-      response.json({
-        result: "ok",
-        data: post,
-        messege: "Query room successfully"
-      });
+      let dataArr = []
+      let likeItem = []
+      let commentItem = []
+      postArr.map((item, index) => {
+        let userId = item.author
+        User.findOne({ _id: userId }).limit(100).sort({}).select({
+          fullName: 1,
+          username: 1,
+          image: 1,
+        }).exec((err, user) => {
+          item['author'] = user;
+          let likeArr = item.likes
+          likeArr.map((item1, index1) => {
+            Like.findOne({ _id: item1 }).limit(100).sort({ user: 1 }).select({
+              user: 1,
+              post: 1,
+            }).exec((err, like) => {
+              likeItem.push(like)
+              if ((index1 + 1) == likeArr.length) {
+                let commentsArr = item.comments
+                commentsArr.map((item2, index2) => {
+                  Comment.findOne({ _id: item2 }).limit(100).sort({ user: 1 }).select({
+                    comment: 1,
+                    post: 1,
+                    author: 1,
+                  }).exec((err, comments) => {
+                    let userId = comments.author
+                    User.findOne({ _id: userId }).limit(100).sort({}).select({
+                      fullName: 1,
+                      username: 1,
+                      image: 1,
+                    }).exec((err, user) => {
+                      comments['author'] = user
+                      commentItem.push(comments)
+                      if ((index2 + 1) == commentsArr.length) {
+                        item['likes'] = likeItem;
+                        item['comments'] = commentItem;
+                        dataArr.push(item)
+                        if (dataArr.length == postArr.length) {
+                          response.json({
+                            result: "ok",
+                            data: {
+                              getFollowedPosts: {
+                                count: `${postArr.length}`,
+                                posts: dataArr,
+                                __typename: "PostsPayload"
+                              }
+                            },
+                            messege: "Query room successfully"
+                          });
+                        }
+                      }
+                    })
+                  })
+                })
+              }
+            })
+          })
+        })
+      })
     }
   });
 });
